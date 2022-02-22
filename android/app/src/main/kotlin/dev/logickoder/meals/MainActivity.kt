084bf25a3b7ac7af08db46598341923e09497b7c
Jeffrey Orazulike <chukwudumebiorazulike@gmail.com>
2022-02-22 18:06:45 +0100

    [feat] created meals project

---
+++ android/app/src/main/kotlin/dev/logickoder/meals/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.logickoder.meals
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+import './utils/strings.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: appName,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: appName),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const SafeArea(
+        child: Center(
+          child: Text(appName),
+        ),
+      ),
+    );
+  }
+}
